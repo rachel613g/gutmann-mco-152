fc57cd0498b44c7f4edf46f2fed4bf2d0191d025
rachel613g <55255752+rachel613g@users.noreply.github.com>
2020-02-25 17:51:33 -0500

    Update CashierTest.java
    added test for broke cashier and notEnoughChange

--- Test/gutmann/cashier/CashierTest.java
+++ Test/gutmann/cashier/CashierTest.java
@@ -26,5 +26,32 @@ public class CashierTest
         assertEquals(customerAssert, change);
         assertEquals(registerAssert, cashier.getCashInRegister());
     }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void brokeCashier() throws IllegalArgumentException
+    {
+        //given
+        Cash customerCash = new Cash(0,0,0,0,3,
+                0,0,0);
+        Cashier cashier = new Cashier(new Cash(0, 0, 0, 0, 0,
+                0, 0, 0));
+
+        //when
+        Cash change = cashier.pay(2.49, customerCash);
+
+    }
+    @Test(expected = IllegalArgumentException)
+    public void notEnoughChange()
+    {
+        //given
+        Cash customerCash = new Cash(0,0,0,0,3,
+                0,0,0);
+        Cashier cashier = new Cashier(new Cash(1, 0, 0, 0, 0,
+                0, 0, 0));
+
+        //when
+        Cash change = cashier.pay(2.49, customerCash);
+
+    }
 
 }
