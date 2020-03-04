b6602ede9152b94b46a35139d0493852ebf1b957
rachel613g <rachelgutmann613@gmail.com>
2020-03-03 21:14:36 -0500

    added ProjectileFrame, Jtext area prints only on one line

--- src/gutmann/projectile/ProjectileFrame.java
+++ src/gutmann/projectile/ProjectileFrame.java
@@ -1,5 +1,63 @@
 package gutmann.projectile;
 
-public class ProjectileFram
+import javax.swing.*;
+import java.awt.*;
+
+public class ProjectileFrame extends JFrame
 {
+    private JTextArea projectileCoordinates;
+    private JTextField velocity;
+    private JTextField angle;
+    private JButton calculateButton;
+    private JLabel velocityLabel;
+    private JLabel angleLabel;
+    private JPanel leftPanel;
+    private Projectile projectile;
+
+
+    public ProjectileFrame()
+    {
+        setSize(300, 400);
+        setTitle("Projectile");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        projectileCoordinates = new JTextArea();
+        velocity = new JTextField();
+        angle = new JTextField();
+        calculateButton = new JButton("Calculate");
+        velocityLabel = new JLabel("Velocity");
+        angleLabel = new JLabel("Angle (in degrees)");
+
+
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new GridLayout(3, 2));
+        leftPanel.add(velocityLabel);
+        leftPanel.add(velocity);
+        leftPanel.add(angleLabel);
+        leftPanel.add(angle);
+        leftPanel.add(calculateButton);
+        calculateButton.addActionListener(actionEvent -> calculateProjectile());
+
+        add(projectileCoordinates, BorderLayout.CENTER);
+        add(leftPanel, BorderLayout.WEST);
+
+    }
+    public void calculateProjectile()
+    {
+        for (int i = 0; i < 31 ; i++)
+        {
+            double angleD = Double.parseDouble(angle.getText());
+            double velocityD = Double.parseDouble(velocity.getText());
+            projectile = new Projectile(angleD, velocityD);
+            projectile.increaseTime(i);
+            projectileCoordinates.append(projectile.toString());
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new ProjectileFrame().setVisible(true);
+    }
 }
