13490fa7f78620ed2c9339df75f72441a9136838
rachel613g <rachelgutmann613@gmail.com>
2020-03-23 12:48:58 -0400

    forgot to instantiate. added now. Still only star component in use. But Frame has all buttons

--- src/gutmann/shapes/ShapeComponent.java
+++ src/gutmann/shapes/ShapeComponent.java
@@ -1,5 +1,39 @@
 package gutmann.shapes;
 
-public class ShapeComponent
+import javax.swing.*;
+import java.awt.*;
+
+public class ShapeComponent extends JComponent
 {
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        int width = getWidth();
+        int height = getHeight();
+
+        int centerX = width/2;
+        int centerY = height/2;
+
+        //makes the center the origin
+        g.translate(centerX, centerY);
+
+        drawStar(g);
+    }
+
+//    public void setStar()
+//    {
+//
+//    }
+
+    private void drawStar(Graphics g)
+    {
+        int[] x1values = new int[]{ -50, 0, 50};
+        int[] y1values = new int[]{0, 50, 0};
+        g.drawPolygon(x1values, y1values, 3);
+
+        int[] x2values = new int[]{-50, 50, 0};
+        int[] y2values = new int[]{25, 25, -25};
+        g.drawPolygon(x2values, y2values, 3);
+    }
 }
--- src/gutmann/shapes/ShapesFrame.java
+++ src/gutmann/shapes/ShapesFrame.java
@@ -1,5 +1,71 @@
-package gutmann.ShapesFrame;
+package gutmann.shapes;
 
-public class ShapesFrame
+import javax.swing.*;
+import java.awt.*;
+
+public class ShapesFrame extends JFrame
 {
+    private ShapeComponent shapeComponent;
+    private JPanel leftPanel;
+   // private JPanel midPanel;
+    private JButton starButton;
+    private JButton diamondRingButton;
+    private JButton balloonButton;
+    private JButton brokenHeartButton;
+    private JButton airplaneButton;
+    private final Dimension buttonDimension = new Dimension(150,100);
+
+
+
+    //add a left panel and buttons inside it
+    //when button is hit paint a difficult shape
+
+    public ShapesFrame()
+    {
+        setSize(500,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Shapes");
+
+        setLayout(new BorderLayout());
+
+        //midPanel = new JPanel();
+        //add(midPanel, BorderLayout.CENTER);
+        //midPanel.setLayout(new FlowLayout());
+        shapeComponent = new ShapeComponent();
+        //add leftPanel for buttons
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        add(leftPanel, BorderLayout.WEST);
+
+        //instantiate, set preferred size and add buttons
+        starButton = new JButton("Star");
+        starButton.setPreferredSize(buttonDimension);
+        diamondRingButton = new JButton("Diamond Ring");
+        diamondRingButton.setPreferredSize(buttonDimension);
+        balloonButton = new JButton("Balloon");
+        balloonButton.setPreferredSize(buttonDimension);
+        brokenHeartButton = new JButton("Broken Heart");
+        brokenHeartButton.setPreferredSize(buttonDimension);
+        airplaneButton = new JButton("Airplane");
+        airplaneButton.setPreferredSize(buttonDimension);
+
+        leftPanel.add(starButton);
+        leftPanel.add(diamondRingButton);
+        leftPanel.add(balloonButton);
+        leftPanel.add(brokenHeartButton);
+        leftPanel.add(airplaneButton);
+
+        starButton.addActionListener(actionEvent -> addComponent());
+    }
+
+    private void addComponent()
+    {
+        add(shapeComponent, BorderLayout.CENTER);
+    }
+
+
+    public static void main(String [] args)
+    {
+        new ShapesFrame().setVisible(true);
+    }
 }
