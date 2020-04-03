1e7cfd624f871c47ea59c5a7ad93b4be4a2111dc
rachel613g <rachelgutmann613@gmail.com>
2020-04-02 21:11:45 -0400

    tests for currentWeather classes

--- Test/gutmann/weather/CurrentWeather.java
+++ Test/gutmann/weather/CurrentWeather.java
@@ -2,4 +2,28 @@ package gutmann.weather;
 
 public class CurrentWeather
 {
+    //all variable names must be the same as the apis
+    String base;
+    int visibility;
+    Coord coord;
+    String name;
+    Main main;
+    Weather weather[];
+
+    class Coord{
+        double lon;
+        double lat;
+    }
+
+    class Main{
+        double temp;
+        double feels_like;
+    }
+
+    class Weather{
+        int id;
+        String main;
+        String description;
+        String icon;
+    }
 }
--- Test/gutmann/weather/ShowCurrentWeather.java
+++ Test/gutmann/weather/ShowCurrentWeather.java
@@ -1,5 +1,41 @@
 package gutmann.weather;
 
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class ShowCurrentWeather
 {
+    //we want to learn how to make a web request
+    public static void main(String[] args) throws IOException
+    {
+        URL url = new URL("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+        URLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream in = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        //we wanted to read the webpage into sout
+        //take 1: inefficient to use inputstream.read() because only reads one character at a time
+//        int c;
+//        while ((c = in.read()) != -1)
+//            System.out.print((char) c);
+
+//take 2: inefficient because just reading the webpage not using the api
+//        String line;
+//        while ((line = reader.readLine()) != null)
+//        {
+//            System.out.println(line);
+//        }
+
+
+     //   what we are supposed to do
+        Gson gson = new Gson();
+        CurrentWeather currentWeather = gson.fromJson(reader, CurrentWeather.class);
+
+        System.out.println(currentWeather.name);
+
+    }
 }
