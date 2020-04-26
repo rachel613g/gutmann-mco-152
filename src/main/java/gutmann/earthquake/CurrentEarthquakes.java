f14044ae4fec07a4a4a1f3dff707d0af08c6c47e
rachel613g <55255752+rachel613g@users.noreply.github.com>
2020-04-26 17:33:27 -0400

    added earthquake package

--- src/main/java/gutmann/earthquake/CurrentEarthquakes.java
+++ src/main/java/gutmann/earthquake/CurrentEarthquakes.java
@@ -2,4 +2,25 @@ package gutmann.earthquake;
 
 public class CurrentEarthquakes
 {
+    Feature features[];
+
+    class Properties {
+        double mag;
+        String place;
+        long time;
+
+        @Override
+        public String toString() {
+            return "Properties{" +
+                    "mag=" + mag +
+                    ", place='" + place + '\'' +
+                    ", time=" + time +
+                    '}';
+        }
+    }
+
+    class Feature {
+        Properties properties;
+    }
+
 }
