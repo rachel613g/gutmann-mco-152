e024f42961198994aafc24483fe87fee69f5fa27
rachel613g <55255752+rachel613g@users.noreply.github.com>
2020-05-04 13:47:29 -0400

    delete neonasa

--- src/main/java/gutmann/neonasademotaketwo/src/main/java/gutmann/neonasademo/NeoService.java
+++
@@ -1,8 +0,0 @@
-package gutmann.neonasademo;
-import retrofit2.Call;
-import retrofit2.http.GET;
-public interface NeoService{
-        @GET("/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY")
-        Call<NeoFeed> getAsteroids();
-}
-
